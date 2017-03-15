bd8d3e68eba830630a311c7da05335212a882dcc
Pitmairen <progrper@gmail.com>
2017-03-15 08:41:30 +0100

    Initial emg to websocket backend connection

---
+++ arduino/emg_connection_interface/emg_connection_interface.ino
@@ -0,0 +1,72 @@
+
+#include "RunningAverage.h"
+
+const int EMG_SENSOR_PIN_INPUT1 = A0;
+const int EMG_SENSOR_PIN_INPUT2 = A1;
+const int EMG_SENSOR_PIN_INPUT3 = A2;
+const int EMG_SENSOR_PIN_INPUT4 = A3;
+
+const int NUMBER_OF_SENSORS = 2;
+const int RUNNING_AVG_SAMPLE_COUNT = 15;
+
+const int EMG_SIGNAL_TRIGGER_THRESHOLD = 12;  // The upper threshold the signal should reach to generate an keyboard input signal
+
+// The lower threshold the signal must reach, after the last generated keyboard input, before again being able to generate a new input signal.
+const int EMG_SIGNAL_RELEASE_THRESHOLD = 8;   
+
+const int sensors[] = {EMG_SENSOR_PIN_INPUT1, EMG_SENSOR_PIN_INPUT2, EMG_SENSOR_PIN_INPUT3, EMG_SENSOR_PIN_INPUT4};
+
+RunningAverage sensorValues[] = {
+  RunningAverage(RUNNING_AVG_SAMPLE_COUNT),
+  RunningAverage(RUNNING_AVG_SAMPLE_COUNT),
+  RunningAverage(RUNNING_AVG_SAMPLE_COUNT),
+  RunningAverage(RUNNING_AVG_SAMPLE_COUNT)
+};
+
+boolean inputTriggerState[] = {false, false, false, false};
+
+void setup() {
+  pinMode(EMG_SENSOR_PIN_INPUT1, INPUT);
+  pinMode(EMG_SENSOR_PIN_INPUT2, INPUT);
+  pinMode(EMG_SENSOR_PIN_INPUT3, INPUT);
+  pinMode(EMG_SENSOR_PIN_INPUT4, INPUT);
+  Serial.begin(9600);
+  for(int i=0; i < NUMBER_OF_SENSORS; i++){
+    resetSensorValueToZero(i);
+  }
+}
+
+void loop() {
+
+  for(int i=0; i < NUMBER_OF_SENSORS; i++){
+    sensorValues[i].addValue(getEMGSignal(i);
+  
+    float sensorValue = sensorValues[i].getAverage();
+    
+    if(sensorValue > EMG_SIGNAL_TRIGGER_THRESHOLD){
+      if(!inputTriggerState[i]){
+        Serial.println(String(i+1));
+        //Serial.println("Trigger " + String(i) + " "+ String(sensorValue));
+        inputTriggerState[i] = true;
+      }
+    }else if(sensorValue < EMG_SIGNAL_RELEASE_THRESHOLD){
+      if(inputTriggerState[i]){
+        //Serial.println("Reset");
+        resetSensorValueToZero(i);
+        inputTriggerState[i] = false;
+      }
+    }
+  }
+  delay(5);
+}
+
+
+int getEMGSignal(int sensorIndex) {
+  return analogRead(sensors[sensorIndex]);
+}
+
+void resetSensorValueToZero(int sensorIndex){
+  sensorValues[i].fillValue(0, RUNNING_AVG_SAMPLE_COUNT);
+}
+
+
---
+++ arduino/emg_websocket_interface.py
@@ -0,0 +1,97 @@
+
+import websocket
+import threading
+import time
+import sys
+
+class WebSocketClient(object):
+
+    def __init__(self, url):
+        self._connection = websocket.WebSocketApp(url,
+                    on_error=self._on_error,
+                    on_open=self._on_open,
+                    on_close=self._on_close)
+
+        self.connected = threading.Event()
+
+    def run_forever(self):
+        self._connection.run_forever()
+
+    def close(self):
+        self._connection.close()
+
+    def send_input(self, input):
+        self._connection.send(input)
+
+    def _on_open(self, ws):
+        print("Connected to websocket interface")
+        self.connected.set()
+
+    def _on_close(self, ws):
+        print("Connection to websocket interface closed")
+        self.connected.clear()
+
+    def _on_error(self, ws, error):
+        print("WebSocket error", error)
+        self.connected.clear()
+
+
+
+def cmd_input(ws_client):
+
+    while True:
+        print("Waiting for websocket to connect")
+        ws_client.connected.wait()
+        while ws_client.connected.is_set():
+            user_input = input("Input type: ").strip()
+            if ws_client.connected.is_set():
+                ws_client.send_input(user_input)
+
+
+def arduino_input(ws_client, usb_device):
+    import serial
+    print("Connecting to arduino on", usb_device)
+    device = serial.Serial(usb_device, 9600)
+
+    while True:
+        if not ws_client.connected.is_set():
+            print("Waiting for websocket to connect")
+            ws_client.connected.wait()
+
+        while ws_client.connected.is_set():
+            user_input = device.readline().strip()
+            if ws_client.connected.is_set():
+                print("Sending input ", user_input)
+                ws_client.send_input(user_input)
+
+
+
+if __name__ == "__main__":
+
+
+    if len(sys.argv) < 2:
+        print("Usage:", sys.argv[0], "<input_websocket_url> [arduino_usb_device]")
+        sys.exit(1)
+
+    ws_url = sys.argv[1]
+    usb_device = None
+
+    if len(sys.argv) > 2:
+        usb_device = sys.argv[2]
+
+    ws_client = WebSocketClient(ws_url)
+    socket_thread = threading.Thread(target=ws_client.run_forever)
+    socket_thread.start()
+    try:
+        if usb_device is not None:
+            arduino_input(ws_client, usb_device)
+        else:
+            cmd_input(ws_client)
+    except KeyboardInterrupt:
+        print("\nShutting down...")
+        ws_client.close()
+        socket_thread.join()
+        print("Shutting complete")
+
+
+
