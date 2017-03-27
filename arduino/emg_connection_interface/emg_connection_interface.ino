9198ed01c975b3132bf26c3599c214804ed0b81d
Pitmairen <progrper@gmail.com>
2017-03-27 11:21:07 +0200

    Implemented auto trigger thresholds for the emg sensors

--- arduino/emg_connection_interface/emg_connection_interface.ino
+++ arduino/emg_connection_interface/emg_connection_interface.ino
@@ -7,14 +7,15 @@ const int EMG_SENSOR_PIN_INPUT3 = A2;
 const int EMG_SENSOR_PIN_INPUT4 = A3;
 
 const int NUMBER_OF_SENSORS = 2;
-const int RUNNING_AVG_SAMPLE_COUNT = 15;
+const int RUNNING_AVG_SAMPLE_COUNT = 10;
+const int THRESHOLD_AVG_SAMPLE_COUNT = 20;
 
-const int EMG_SIGNAL_TRIGGER_THRESHOLD = 12;  // The upper threshold the signal should reach to generate an keyboard input signal
-
-// The lower threshold the signal must reach, after the last generated keyboard input, before again being able to generate a new input signal.
-const int EMG_SIGNAL_RELEASE_THRESHOLD = 8;   
-
+  
 const int sensors[] = {EMG_SENSOR_PIN_INPUT1, EMG_SENSOR_PIN_INPUT2, EMG_SENSOR_PIN_INPUT3, EMG_SENSOR_PIN_INPUT4};
+
+const int THRESHOLD_SAMPLE_COUNTER_TARGET = 11;
+
+int thresholdSampleCounter[] = {0, 0, 0, 0}; // Used to only sample the threshold values every THRESHOLD_SAMPLE_COUNTER_TARGET times through the main loop
 
 RunningAverage sensorValues[] = {
   RunningAverage(RUNNING_AVG_SAMPLE_COUNT),
@@ -23,7 +24,23 @@ RunningAverage sensorValues[] = {
   RunningAverage(RUNNING_AVG_SAMPLE_COUNT)
 };
 
+RunningAverage thresholdValuesHigh[] = {
+  RunningAverage(THRESHOLD_AVG_SAMPLE_COUNT),
+  RunningAverage(THRESHOLD_AVG_SAMPLE_COUNT),
+  RunningAverage(THRESHOLD_AVG_SAMPLE_COUNT),
+  RunningAverage(THRESHOLD_AVG_SAMPLE_COUNT)
+};
+
+RunningAverage thresholdValuesLow[] = {
+  RunningAverage(THRESHOLD_AVG_SAMPLE_COUNT),
+  RunningAverage(THRESHOLD_AVG_SAMPLE_COUNT),
+  RunningAverage(THRESHOLD_AVG_SAMPLE_COUNT),
+  RunningAverage(THRESHOLD_AVG_SAMPLE_COUNT)
+};
+
+
 boolean inputTriggerState[] = {false, false, false, false};
+
 
 void setup() {
   pinMode(EMG_SENSOR_PIN_INPUT1, INPUT);
@@ -33,32 +50,46 @@ void setup() {
   Serial.begin(9600);
   for(int i=0; i < NUMBER_OF_SENSORS; i++){
     resetSensorValueToZero(i);
+    resetThresholdValueToZero(i);
   }
 }
+
 
 void loop() {
 
   for(int i=0; i < NUMBER_OF_SENSORS; i++){
-    sensorValues[i].addValue(getEMGSignal(i);
-  
+    sensorValues[i].addValue(getEMGSignal(i));
+
     float sensorValue = sensorValues[i].getAverage();
+
+    // For debugging uncomment and use the serial plotter to see the signal values. (Remember to comment out the signal trigger println a few lines below) 
+    //Serial.println(String(sensorValue) + "  " + thresholdValuesHigh[i].getAverage() + " " + thresholdValuesLow[i].getAverage());
     
-    if(sensorValue > EMG_SIGNAL_TRIGGER_THRESHOLD){
+    if(sensorValue > thresholdValuesHigh[i].getAverage()){
       if(!inputTriggerState[i]){
         Serial.println(String(i+1));
-        //Serial.println("Trigger " + String(i) + " "+ String(sensorValue));
         inputTriggerState[i] = true;
       }
-    }else if(sensorValue < EMG_SIGNAL_RELEASE_THRESHOLD){
+
+    }else if(sensorValue < thresholdValuesLow[i].getAverage()){
       if(inputTriggerState[i]){
-        //Serial.println("Reset");
-        resetSensorValueToZero(i);
+        //Serial.println("Reset " + (i+1));
+        resetSensorValueToZero(i); // Reset sensor values to zero to reduce the bounce in the signal that often occurs after the signal triggers
         inputTriggerState[i] = false;
       }
     }
+
+    // Update threshold values
+    if(++thresholdSampleCounter[i] == THRESHOLD_SAMPLE_COUNTER_TARGET){
+      thresholdValuesHigh[i].addValue(sensorValue*1.7);
+      thresholdValuesLow[i].addValue(sensorValue);
+      thresholdSampleCounter[i] = 0;
+    }
+
   }
-  delay(5);
+  delay(20);
 }
+
 
 
 int getEMGSignal(int sensorIndex) {
@@ -68,5 +99,9 @@ int getEMGSignal(int sensorIndex) {
 void resetSensorValueToZero(int i){
   sensorValues[i].fillValue(0, RUNNING_AVG_SAMPLE_COUNT);
 }
+void resetThresholdValueToZero(int i){
+  thresholdValuesHigh[i].fillValue(0, THRESHOLD_AVG_SAMPLE_COUNT);
+  thresholdValuesLow[i].fillValue(0, THRESHOLD_AVG_SAMPLE_COUNT);
 
+}
 
